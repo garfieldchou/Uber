5f2f6febe1cd3b677e0a5a954df2eab3b324a233
shaudai <shaudai0402@hotmail.com>
2017-02-14 17:45:04 +0800

    'Call Uber' button on the top of the rider activity for post a request  with username and location
    1, Use ParseGeoPoint to save the location
    2, Update corresponding button text

--- ParseStarterProject/src/main/java/com/parse/starter/RiderActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/RiderActivity.java
@@ -13,6 +13,10 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -20,6 +24,11 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.ParseException;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class RiderActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -28,6 +37,51 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
     LocationManager locationManager;
 
     LocationListener locationListener;
+
+    Button callUberButton;
+
+    public void callUber (View view) {
+
+        Log.i("Info", "Call Uber");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
+
+            Location lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+            if (lastKnownLocation != null) {
+
+                ParseObject request = new ParseObject("request");
+
+                request.put("username", ParseUser.getCurrentUser().getUsername());
+
+                ParseGeoPoint parseGeoPoint = new ParseGeoPoint(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude());
+
+                request.put("location", parseGeoPoint);
+
+                request.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+
+                        if (e == null) {
+
+                            callUberButton.setText("Cancel Uber");
+
+                        }
+                    }
+                });
+
+            } else {
+
+                Toast.makeText(this, "Could not find location. Please try again later.", Toast.LENGTH_SHORT).show();
+                
+            }
+        }
+
+
+
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -70,6 +124,8 @@ public class RiderActivity extends FragmentActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        callUberButton = (Button) findViewById(R.id.callUberButton);
     }
 
 
