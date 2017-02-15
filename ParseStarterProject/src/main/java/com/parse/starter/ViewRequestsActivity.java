e319fc3251ab5a3994e4fd85ce3029e77b31fc42
shaudai <garfield_chou@asus.com>
2017-02-15 12:50:47 +0800

    Add a list view at request view activity for test

--- ParseStarterProject/src/main/java/com/parse/starter/ViewRequestsActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/ViewRequestsActivity.java
@@ -2,12 +2,32 @@ package com.parse.starter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class ViewRequestsActivity extends AppCompatActivity {
+
+    ListView requestListView;
+    ArrayList<String> requests = new ArrayList<String>();
+    ArrayAdapter arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_requests);
+
+        setTitle("Nearby Requests");
+
+        requestListView = (ListView) findViewById(R.id.requestListView);
+
+        requests.add("test");
+
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, requests);
+
+        requestListView.setAdapter(arrayAdapter);
+
     }
 }
